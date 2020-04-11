b0e2777f5c9b28d693eff7f82ef72da0b2baa155
yangning <yangning110302@163.com>
2020-04-11 21:40:36 +0800

    项目初始化

---
+++ src/main/java/com/qlx/QlxPayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qlx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlxPayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlxPayServiceApplication.class, args);
+    }
+
+}
